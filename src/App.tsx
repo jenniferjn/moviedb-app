5ad61e3a3535a58400f6ef20d9beee527bc2fced
Jennifer <djennifer.2208@gmail.com>
2023-08-19 14:42:52 +0700

    Put Featured into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Featured from './components/Featured';
 import Navigation from './components/Navigation';
 
 function App() {
   return (
     <>
       <Navigation></Navigation>
+      <Featured></Featured>
     </>
   );
 }
